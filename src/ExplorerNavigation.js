42544d6e9c2622d9bda6b021ab0e026fd14bb97d
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-11-28 17:48:45 +0100

    Add unique color to Navigation Item Icons

--- src/ExplorerNavigation.js
+++ src/ExplorerNavigation.js
@@ -5,11 +5,11 @@ const ExplorerNavigation = () => {
   return (
     <aside className="flex-shrink-0 p-2 w-52 border-r border-gray-700 flex flex-col space-y-1 overflow-y-auto">
       <ExplorerNavigationButton active>
-        <HomeIcon className="w-5 h-5" />
+        <HomeIcon className="w-5 h-5 text-yellow-300" />
         <span>Desktop</span>
       </ExplorerNavigationButton>
       <ExplorerNavigationButton>
-        <DownloadIcon className="w-5 h-5" />
+        <DownloadIcon className="w-5 h-5 text-blue-400" />
         <span>Download</span>
       </ExplorerNavigationButton>
     </aside>
